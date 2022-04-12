3aaf994e3dd622a287323165851ad8f89a483f05
sachin_avchar <sachinavachar143@gmail.com>
2022-04-12 19:20:36 +0530

    again changed alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -49,7 +49,7 @@ export class AppComponent implements OnInit {
         this.dataSource.sort = this.sort;
       },
       error: () => {
-        alert('Error while...');
+        alert('Error while Updating ...');
       },
     });
   }
